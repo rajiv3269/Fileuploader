80af2ef63a2861c38716fb637bee8665c8d8a699
430431 <430431@PC217159.cts.com>
2016-02-15 18:52:12 +0530

    V1.1

---
+++ src/org/fileupload/spring/Controller/LoginController.java
@@ -0,0 +1,14 @@
+package org.fileupload.spring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = "/login",method = RequestMethod.GET)
+	public String getForm(){
+		return "Login";
+	}
+}
